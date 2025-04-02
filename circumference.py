7a6ca6b5aa717b957b3daa51a26635905c57aec2
charlottecode <charlottedesigner2013@gmail.com>
2025-04-01 20:15:38 -0500

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,13 @@
+pi = 3.14159
+
+print("Hello! Let's calculate the circumference.")
+
+radius_text = input("Enter the radius: ")
+
+radius_number = float(radius_text)
+
+circumference = 2 * pi * radius_number
+
+print("The circumference is:", circumference)
+
+print("All done! :)")
